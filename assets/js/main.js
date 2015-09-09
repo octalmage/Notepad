d6f149472b1a1f59e808d6780fe069c4a49e8e1e
Jason Stallings <jason@stallin.gs>
2015-09-09 06:43:09 -0500

    The start.
    It’s looking pretty good!

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*
+ *  _   _       _                       _ 
+ * | \ | | ___ | |_ ___ _ __   __ _  __| |
+ * |  \| |/ _ \| __/ _ \ '_ \ / _` |/ _` |
+ * | |\  | (_) | ||  __/ |_) | (_| | (_| |
+ * |_| \_|\___/ \__\___| .__/ \__,_|\__,_|
+ *                     |_|                
+ * By: Jason Stallings
+ */
+
+var gui = require("nw.gui");
+
+//Get the current window.
+var win = gui.Window.get();
+
+//Show the window when the app opens.
+win.show();
